fa46e1ccb8358902a928c60941bc7389db3ca0a3
shenxt <kagawabale@163.com>
2017-08-18 20:47:35 +0800

    fix

---
+++ R/test.R
@@ -0,0 +1,12 @@
+#' @title test
+#' @description test
+#' @author Xiaotao Shen
+#' \email{shenxt1990@@163.com}
+#' @param x x
+#' @param y y
+#' @return x+y
+#' @export
+
+test <- function(x, y){
+  x + y
+}
