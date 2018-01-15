88d30c958505c9d1ec926cad897e7c2b70a91419
omicschina <omicschina@163.com>
2018-01-15 15:11:02 +0800

    version0.99.16

--- R/test.R
+++ R/test.R
@@ -14,16 +14,16 @@ test <- function(x, y){
 
 
 .onAttach <- function(libname, pkgname) {
-packageStartupMessage("analysisReport version 0.99.14,
-which is used for MetDNA 0.99.14.
+packageStartupMessage("analysisReport version 0.99.16,
+which is used for MetDNA 0.99.16.
 Author: Jasper Shen.
 Maintainer: Jasper Shen (shenxt1990@163.com).
 News:
 o Change the logo.")
 }
 
-packageStartupMessage("analysisReport version 0.99.14,
-which is used for MetDNA 0.99.14.
+packageStartupMessage("analysisReport version 0.99.16,
+which is used for MetDNA 0.99.16.
 Author: Jasper Shen.
 Maintainer: Jasper Shen (shenxt1990@163.com).
 News:
